bc9f0472f5645becb9c2d8dd2acdc6241c62d4f6
Sumant Pandey <sumantpandey9941@gmail.com>
2025-07-24 16:05:21 +0530

    Update Postcreate.jsx

--- frontend/src/Postcreate.jsx
+++ frontend/src/Postcreate.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 import { AiOutlineSend } from "react-icons/ai";
 import { IoMdTrendingUp } from "react-icons/io";
 import { io } from "socket.io-client";
-let socket = io("http://localhost:4000");
+let socket = io("https://linkdin-backend-jo4v.onrender.com");
 function Postcreate({
   id,
   author,
