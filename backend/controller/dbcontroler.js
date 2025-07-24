2fc039001923a28e01af3e489c7a23170bbd6a85
Sumant Pandey <sumantpandey9941@gmail.com>
2025-07-24 18:35:30 +0530

    Update dbcontroler.js

--- backend/controller/dbcontroler.js
+++ backend/controller/dbcontroler.js
@@ -33,7 +33,6 @@ export const signup=async(req,res)=>{
   let token=generatetoken(user._id)
   res.cookie("token",token,{
     httpOnly:true,
-   F
     secure: true,
     sameSite:"None",
     maxAge:7*24*60*60*1000
@@ -66,7 +65,6 @@ export const login=async(req,res)=>{
  
   res.cookie("token",token,{
     httpOnly:true,
-  
      secure: true,
     sameSite:"None",
     maxAge:7*24*60*60*1000
